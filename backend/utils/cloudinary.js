585235dcc8ee2de91f419739985174f0900be306
JUDHI3134 <judhistirbehera456@gmail.com>
2025-02-02 11:54:27 +0530

    bug fixed register controller

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -18,7 +18,8 @@ const uploadOnCloudinary = async (localFilePath) => {
             resource_type: "auto"
         })
 
-        console.log("file upload on cloudinary successfully" , response.url)
+        // console.log("file upload on cloudinary successfully" , response.url)
+        fs.unlinkSync(localFilePath)
         
         return response;
 
